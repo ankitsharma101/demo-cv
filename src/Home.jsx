aef86bec05a4fcb9f7175f59641a71cbfa92b922
Ankit Sharma <ankitsharma101.exe@gmail.com>
2021-01-09 11:06:11 +0530

    Intial Commit

---
+++ src/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyCard from "./MainCard.jsx";
+import Navbar from "./Navbar.jsx";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import AboutCard from './about-card.jsx';
+import EducationCard from './education-card.jsx';
+
+function Home(){
+    return<div class="container">
+  <Navbar />
+  <div class="row">
+    <div class="col-lg">
+      <MyCard />
+    </div>
+    <div class="col-lg about-card">
+      <AboutCard />
+    </div>
+  </div>
+</div>
+}
+
+export default Home;
